2d7b64c8ba80356750238bb930ed4bc733a63d9d
Daniel de Souza Neris <danielneris01@gmail.com>
2021-02-28 18:00:01 -0300

    feature(Countdown): created component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import  Head  from 'next/head'
 import { CompleatedChallenges } from '../components/CompleatedChallenges'
+import { Countdown } from '../components/Countdown'
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
 
@@ -7,12 +9,17 @@ import styles from '../styles/components/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Start | do.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompleatedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
